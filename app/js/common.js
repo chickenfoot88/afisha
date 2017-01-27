96b198eabab4bb5b320668d877203ae0a0b076ca
Adel <adel55@mail.ru>
2017-01-27 10:16:51 +0300

    js

---
+++ app/js/common.js
@@ -0,0 +1,57 @@
+$(function() {
+
+  setBtnWidth();
+
+  //Кастомный скрол на странице
+  $('.cities-list').customScroll({
+    prefix: 'custom-scroll_',
+    barHtml: '<div />',
+    horizontal: false,
+  });
+
+
+//Выпадающий список городов
+   $('.cities-btn').click(function(){
+     $(".cities-list").slideToggle('fast');
+   });
+
+   $('ul.cities-list li').click(function(){
+     var tx = $(this).html();
+    $(".cities-list").slideUp('fast');
+    $(".cities-choosen span").html(tx);
+
+    setBtnWidth();
+   });
+
+   //Скрывает список городов, если кликает не по списку
+   $(document).mouseup(function(e) {
+     var div = $(".cities-btn");
+     if(!div.is(e.target) && div.has(e.target).length === 0) {
+       $(".cities-list").slideUp("fast");
+     }
+   })
+
+
+//Изменение ширины кнопки выбора города
+   function setBtnWidth() {
+     $(".cities-choosen").css('width', function() {
+       return parseInt($(".cities-choosen span").css('width')) + 50 + "px";
+     });
+   };
+
+//Слайд-шоу
+  var owl = $(".slider");
+   owl.owlCarousel({
+     loop: true,
+     items: 1,
+     itemClass: "slide-wrap",
+     navText: ""
+   });
+   $(".next").click(function() {
+     owl.trigger('next.owl.carousel');
+   })
+   $(".prev").click(function() {
+     owl.trigger('prev.owl.carousel');
+   })
+
+});
